629646cc96526b409ba42c4cb995a967c6ebdbd6
Sushant Mimani <sushant.mimani@gmail.com>
2020-04-14 12:20:16 -0700

    update scirpt

--- scripts/sample.sh
+++ scripts/sample.sh
@@ -2,6 +2,11 @@
 
 FULL_VERSION=$(mvn -q -DforceStdout help:evaluate -Dexpression=project.version)
 BASE_VERSION=`echo ${FULL_VERSION} | cut -f1 -d'-'`
-BRANCH="release-${BASE_VERSION}"
+BRANCH="${BASE_VERSION}-rc1"
 
 mvn release:branch -DbranchName=${BRANCH}
+
+TAG="v${BRANCH}"
+git tag -f ${TAG}
+git push origin "${BRANCH}"
+git push origin ${TAG}
