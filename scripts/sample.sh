c21d21fcc6d6eea421868c9dec8320a3398fcf59
Sushant Mimani <sushant.mimani@gmail.com>
2020-04-14 12:29:33 -0700

    test

--- scripts/sample.sh
+++ scripts/sample.sh
@@ -1,12 +1,23 @@
 #!/usr/bin/env bash
 
+REPO=`git remote -v | grep origin | grep fetch | awk '{print $2}'`
 FULL_VERSION=$(mvn -q -DforceStdout help:evaluate -Dexpression=project.version)
 BASE_VERSION=`echo ${FULL_VERSION} | cut -f1 -d'-'`
-BRANCH="${BASE_VERSION}-rc1"
+BRANCH="release-${BASE_VERSION}"
 
-mvn release:branch -DbranchName=${BRANCH}
+git ls-remote --exit-code --heads ${REPO} ${BRANCH} > /dev/null
+if [[ "$?" == "2" ]]; then
+    mvn release:branch -DbranchName=${BRANCH}
+    git checkout -b "${BRANCH}"
+else
+   git fetch ${REPO} ${BRANCH}
+   git checkout "${BRANCH}"
+   FULL_VERSION=$(mvn -q -DforceStdout help:evaluate -Dexpression=project.version)
+   RC=$(echo "$FULL_VERSION" | cut -f2 -d'-' | tr -d [:alpha:])
+   NEW_VERSION="${BASE_VERSION}-rc$((${RC}+1))"
+fi
 
-TAG="v${BRANCH}"
+TAG="v${NEW_VERSION}"
 git tag -f ${TAG}
 git push origin "${BRANCH}"
 git push origin ${TAG}
