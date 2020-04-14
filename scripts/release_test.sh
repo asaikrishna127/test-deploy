59f5d761427b1f982dfb53afaff7719b9fe85373
Sushant Mimani <sushant.mimani@gmail.com>
2020-04-14 11:50:31 -0700

    Update scripts

--- scripts/release_test.sh
+++ scripts/release_test.sh
@@ -1,19 +1,18 @@
 #!/usr/bin/env bash
 REPO=`git remote -v | grep origin | grep fetch | awk '{print $2}'`
 FULL_VERSION=$(mvn -q -DforceStdout help:evaluate -Dexpression=project.version)
-BASE_VERSION=`echo $FULL_VERSION | cut -f1 -d'-'`
-echo $BASE_VERSION
+BASE_VERSION=`echo ${FULL_VERSION} | cut -f1 -d'-'`
 BRANCH="release-${BASE_VERSION}"
 
 git ls-remote --exit-code --heads ${REPO} ${BRANCH} > /dev/null
-if [ "$?" == "2" ]; then
+if [[ "$?" == "2" ]]; then
    # No branch exists, so this is the first RC for this version
    NEW_VERSION="${BASE_VERSION}-rc1"
 
    # Bump master
    git checkout master
-   MINOR_VERSION=`echo $BASE_VERSION | cut -f2 -d.`
-   NEW_BASE_VERSION=`echo $BASE_VERSION | cut -f1 -d.`.$((${MINOR_VERSION}+1)).0
+   MINOR_VERSION=`echo ${BASE_VERSION} | cut -f2 -d.`
+   NEW_BASE_VERSION=`echo ${BASE_VERSION} | cut -f1 -d.`.$((${MINOR_VERSION}+1)).0
    mvn versions:set versions:commit -DnewVersion="${NEW_BASE_VERSION}-SNAPSHOT"
    git add pom.xml
    git commit -m "Master version bump to ${NEW_BASE_VERSION}-SNAPSHOT"
@@ -23,7 +22,7 @@ if [ "$?" == "2" ]; then
    git checkout -b "${BRANCH}"
 else
    # Checkout the branch, and figure out our new RC version
-   git fetch $REPO ${BRANCH}
+   git fetch ${REPO} ${BRANCH}
    git checkout "${BRANCH}"
    FULL_VERSION=$(mvn -q -DforceStdout help:evaluate -Dexpression=project.version)
    RC=$(echo "$FULL_VERSION" | cut -f2 -d'-' | tr -d [:alpha:])
@@ -38,4 +37,3 @@ TAG="v${NEW_VERSION}"
 git tag -f ${TAG}
 git push origin "${BRANCH}"
 git push origin ${TAG}
-echo $NEW_VERSION
