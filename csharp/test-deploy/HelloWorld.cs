4a7fdd4c4e82173dd1a3814b45c932d7daa33d6a
Sushant Mimani <sushant.mimani@gmail.com>
2020-04-16 16:14:52 -0700

    Add C#. Reset version

---
+++ csharp/test-deploy/HelloWorld.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace com.github.sushantmimani
+{
+    public static class HelloWorld
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine ("Hello World");
+        }
+    }
+}
