b6f5df4bd2d9d063f54d063e1bf88a12a2a6d5fe
Sushant Mimani <sushant.mimani@gmail.com>
2020-04-20 16:11:27 -0700

    test GPG key

--- java/test-deploy-1/scripts/release_prod.sh
+++ java/test-deploy-1/scripts/release_prod.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 echo "Release prod artifact"
 BASE_VERSION=$(mvn -q -DforceStdout help:evaluate -Dexpression=project.version)
-echo $BASE_VERSION
+echo ${BASE_VERSION}
 
-mvn -DskipTests deploy
+echo ${PRIVATE_GPG_KEY} | base64 --decode | gpg --batch --no-tty --import --yes
+
+mvn -DskipTests deploy -Prelease
 
 # Bump master
 git checkout master
-MINOR_VERSION=`echo $BASE_VERSION | cut -f2 -d.`
-NEW_BASE_VERSION=`echo $BASE_VERSION | cut -f1 -d.`.$((${MINOR_VERSION}+1)).0
+MINOR_VERSION=`echo ${BASE_VERSION} | cut -f2 -d.`
+NEW_BASE_VERSION=`echo ${BASE_VERSION} | cut -f1 -d.`.$((${MINOR_VERSION}+1)).0
 mvn versions:set versions:commit -DnewVersion="${NEW_BASE_VERSION}-SNAPSHOT"
 git add pom.xml
 git commit -m "Master version bump to ${NEW_BASE_VERSION}-SNAPSHOT"
--- java/test-deploy/scripts/release_snapshot.sh
+++ java/test-deploy/scripts/release_snapshot.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 echo "Deploying snapshot"
 mvn -DskipTests -s ../../.circleci/settings.xml deploy
+
+
+echo ${PRIVATE_GPG_KEY} | base64 --decode | gpg --batch --no-tty --import --yes
