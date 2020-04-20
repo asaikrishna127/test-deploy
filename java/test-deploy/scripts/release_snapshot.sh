65285d0a13abe0cb5f0e08004e0226beb739ba25
Sushant Mimani <sushant.mimani@gmail.com>
2020-04-20 16:37:08 -0700

    Reset version

--- java/test-deploy/scripts/release_snapshot.sh
+++ java/test-deploy/scripts/release_snapshot.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 echo "Deploying snapshot"
 mvn -DskipTests -s ../../.circleci/settings.xml deploy
-
-
-echo ${PRIVATE_GPG_KEY} | base64 --decode | gpg --batch --no-tty --import --yes
