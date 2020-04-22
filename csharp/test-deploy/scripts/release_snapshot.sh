5a30d16838cd2a98548b8c961c9e5af35515fe95
Sushant Mimani <sushant.mimani@gmail.com>
2020-04-21 21:59:05 -0700

    use timestamp

--- csharp/test-deploy/scripts/release_snapshot.sh
+++ csharp/test-deploy/scripts/release_snapshot.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 echo "Deploying snapshot"
-BASE_VERSION=$(xmllint --xpath "//Project/PropertyGroup/Version/text()" Directory.Build.props)
-echo ${BASE_VERSION}
+FULL_VERSION=$(xmllint --xpath "//Project/PropertyGroup/Version/text()" Directory.Build.props)
+echo ${FULL_VERSION}
+
+TIME_STAMP=$(date "+%Y%m%d%H%M%S")
+PACKAGE_VERSION=${FULL_VERSION}.${TIME_STAMP}
 
 export NUGET_SOURCE=$1
-dotnet pack -c Release --no-build
+dotnet pack -c Release -p:PackageVersion=${PACKAGE_VERSION} --no-build
 
-# Delete existing packages from nuget to support updating -alpha builds and idempotency for others (i.e. mimic mvn deploy semantics).
-# This is some ugly bash magic to split package name and version tuples to pipe them properly into dotnet nuget delete.
-find . -name *${BASE_VERSION}.nupkg  | xargs -L1 -I '{}' dotnet nuget delete {} -k ${NUGET_KEY} -s ${NUGET_SOURCE}
-find . -name *${BASE_VERSION}.nupkg  | xargs -L1 -I '{}' dotnet nuget push {} -k ${NUGET_KEY} -s ${NUGET_SOURCE}
+find . -name *${PACKAGE_VERSION}.nupkg  | xargs -L1 -I '{}' dotnet nuget push {} -k ${NUGET_KEY} -s ${NUGET_SOURCE}
