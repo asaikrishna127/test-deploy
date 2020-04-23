22b422b6c3cd9003cedd97d6b99ef5317b06d291
Sushant Mimani <sushant.mimani@gmail.com>
2020-04-23 16:08:12 -0700

    update java scripts

--- java/HelloWorld/scripts/release_prod.sh
+++ java/HelloWorld/scripts/release_prod.sh
@@ -1,16 +1,13 @@
 #!/usr/bin/env bash
 echo "Release prod artifact"
 BASE_VERSION=$(mvn -q -DforceStdout help:evaluate -Dexpression=project.version)
-echo ${BASE_VERSION}
+ARTIFACT_NAME=$(mvn -q -DforceStdout help:evaluate -Dexpression=project.artifactId)
+TAG=`echo java/${ARTIFACT_NAME}/v${BASE_VERSION}`
 
 echo ${PRIVATE_GPG_KEY} | base64 --decode | gpg --batch --no-tty --import --yes
 
 mvn -DskipTests -s ../../.circleci/settings.xml deploy -Prelease
 
-# Bump master
-git checkout master
-MINOR_VERSION=`echo ${BASE_VERSION} | cut -f2 -d.`
-NEW_BASE_VERSION=`echo ${BASE_VERSION} | cut -f1 -d.`.$((${MINOR_VERSION}+1)).0
-mvn versions:set versions:commit -DnewVersion="${NEW_BASE_VERSION}-SNAPSHOT"
-git add pom.xml
-git commit -m "Master version bump to ${NEW_BASE_VERSION}-SNAPSHOT [ci skip]"
+# Create tag
+git tag -f ${TAG}
+git push origin ${TAG}
