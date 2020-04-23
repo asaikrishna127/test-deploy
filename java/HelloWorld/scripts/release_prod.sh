32106c5d756de0b851dfa8e75159fd8ee6081b09
Sushant Mimani <sushant.mimani@gmail.com>
2020-04-23 16:59:10 -0700

    Do not deploy if tag exists

--- java/HelloWorld/scripts/release_prod.sh
+++ java/HelloWorld/scripts/release_prod.sh
@@ -4,10 +4,13 @@ BASE_VERSION=$(mvn -q -DforceStdout help:evaluate -Dexpression=project.version)
 ARTIFACT_NAME=$(mvn -q -DforceStdout help:evaluate -Dexpression=project.artifactId)
 TAG=`echo java/${ARTIFACT_NAME}/v${BASE_VERSION}`
 
-echo ${PRIVATE_GPG_KEY} | base64 --decode | gpg --batch --no-tty --import --yes
+RESULT=$(git tag -l ${TAG})
+if [[ "$RESULT" != ${TAG} ]]; then
+    echo ${PRIVATE_GPG_KEY} | base64 --decode | gpg --batch --no-tty --import --yes
 
-mvn -DskipTests -s ../../.circleci/settings.xml deploy -Prelease
+    mvn -DskipTests -s ../../.circleci/settings.xml deploy -Prelease
 
-# Create tag
-git tag -f ${TAG}
-git push origin ${TAG}
+    # Create tag
+    git tag -f ${TAG}
+    git push origin ${TAG}
+fi
