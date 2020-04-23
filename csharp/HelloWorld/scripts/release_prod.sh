1bdf1756698dc1283aad8cae03752c440e75d106
Sushant Mimani <sushant.mimani@gmail.com>
2020-04-23 16:19:12 -0700

    Update c# scripts

--- csharp/HelloWorld/scripts/release_prod.sh
+++ csharp/HelloWorld/scripts/release_prod.sh
@@ -1,17 +1,14 @@
 #!/usr/bin/env bash
 echo "Release prod artifact"
 BASE_VERSION=$(xmllint --xpath "//Project/PropertyGroup/Version/text()" Directory.Build.props)
-echo ${BASE_VERSION}
+ARTIFACT_NAME=$(xmllint --xpath "//package/metadata/title/text()" HelloWorld.nuspec)
+TAG=`echo csharp/${ARTIFACT_NAME}/v${BASE_VERSION}`
 
 export NUGET_SOURCE=$1
 dotnet pack -c Release --no-build
 
 find . -name *${BASE_VERSION}.nupkg  | xargs -L1 -I '{}' dotnet nuget push {} -k ${NUGET_KEY} -s ${NUGET_SOURCE}
 
-# Bump master
-git checkout master
-MINOR_VERSION=`echo $BASE_VERSION | cut -f2 -d.`
-NEW_BASE_VERSION=`echo $BASE_VERSION | cut -f1 -d.`.$((${MINOR_VERSION}+1)).0
-setversion "${NEW_BASE_VERSION}-alpha" Directory.Build.props
-git add Directory.Build.props
-git commit -m "Master version bump to ${NEW_BASE_VERSION}-alpha [skip ci]"
+# Create tag
+git tag -f ${TAG}
+git push origin ${TAG}
