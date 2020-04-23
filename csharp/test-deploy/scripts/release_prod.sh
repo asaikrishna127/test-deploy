a029af575abc252f30a7fd1bb0ce4ead552a256f
Sushant Mimani <sushant.mimani@gmail.com>
2020-04-23 11:39:32 -0700

    Release all 4 artifacts

--- csharp/test-deploy/scripts/release_prod.sh
+++ csharp/test-deploy/scripts/release_prod.sh
@@ -9,6 +9,8 @@ dotnet pack -c Release --no-build
 find . -name *${BASE_VERSION}.nupkg  | xargs -L1 -I '{}' dotnet nuget push {} -k ${NUGET_KEY} -s ${NUGET_SOURCE}
 
 # Bump master
+git checkout master
+git pull origin master
 MINOR_VERSION=`echo $BASE_VERSION | cut -f2 -d.`
 NEW_BASE_VERSION=`echo $BASE_VERSION | cut -f1 -d.`.$((${MINOR_VERSION}+1)).0
 dotnet tool install -g dotnet-setversion
@@ -16,5 +18,4 @@ export PATH="$PATH:/root/.dotnet/tools"
 setversion "${NEW_BASE_VERSION}-alpha" Directory.Build.props
 git add Directory.Build.props
 git commit -m "Master version bump to ${NEW_BASE_VERSION}-alpha [skip ci]"
-git pull origin master
 git push origin master
