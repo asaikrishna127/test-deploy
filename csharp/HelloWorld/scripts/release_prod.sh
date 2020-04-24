08dcbad45a39feccca2d7f47d71c53fe0ef522a7
Sushant Mimani <sushant.mimani@gmail.com>
2020-04-23 17:01:48 -0700

    [skip ci]

--- csharp/HelloWorld/scripts/release_prod.sh
+++ csharp/HelloWorld/scripts/release_prod.sh
@@ -4,11 +4,14 @@ BASE_VERSION=$(xmllint --xpath "//Project/PropertyGroup/Version/text()" Director
 ARTIFACT_NAME=$(xmllint --xpath "//package/metadata/title/text()" HelloWorld.nuspec)
 TAG=`echo csharp/${ARTIFACT_NAME}/v${BASE_VERSION}`
 
-export NUGET_SOURCE=$1
-dotnet pack -c Release --no-build
+RESULT=$(git tag -l ${TAG})
+if [[ "$RESULT" != ${TAG} ]]; then
+    export NUGET_SOURCE=$1
+    dotnet pack -c Release --no-build
 
-find . -name *${BASE_VERSION}.nupkg  | xargs -L1 -I '{}' dotnet nuget push {} -k ${NUGET_KEY} -s ${NUGET_SOURCE}
+    find . -name *${BASE_VERSION}.nupkg  | xargs -L1 -I '{}' dotnet nuget push {} -k ${NUGET_KEY} -s ${NUGET_SOURCE}
 
-# Create tag
-git tag -f ${TAG}
-git push origin ${TAG}
+    # Create tag
+    git tag -f ${TAG}
+    git push origin ${TAG}
+fi
